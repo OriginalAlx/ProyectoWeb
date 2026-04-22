ffb1d292301b92757bd8fbb5debfbb882bb73067
alexp <alexp@DESKTOP-DPV3V40>
2026-04-22 18:26:34 -0300

    Segundo Commit

--- src/main/java/com/libreriaedex/model/Producto.java
+++ src/main/java/com/libreriaedex/model/Producto.java
@@ -12,5 +12,63 @@ public class Producto {
     private String nombre_producto, categoria_producto, imagen_producto_url;
     
     private double precio_unitario;
+
+    public int getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(int id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public int getStock_minimo() {
+        return stock_minimo;
+    }
+
+    public void setStock_minimo(int stock_minimo) {
+        this.stock_minimo = stock_minimo;
+    }
+
+    public int getStock_maximo() {
+        return stock_maximo;
+    }
+
+    public void setStock_maximo(int stock_maximo) {
+        this.stock_maximo = stock_maximo;
+    }
+
+    public String getNombre_producto() {
+        return nombre_producto;
+    }
+
+    public void setNombre_producto(String nombre_producto) {
+        this.nombre_producto = nombre_producto;
+    }
+
+    public String getCategoria_producto() {
+        return categoria_producto;
+    }
+
+    public void setCategoria_producto(String categoria_producto) {
+        this.categoria_producto = categoria_producto;
+    }
+
+    public String getImagen_producto_url() {
+        return imagen_producto_url;
+    }
+
+    public void setImagen_producto_url(String imagen_producto_url) {
+        this.imagen_producto_url = imagen_producto_url;
+    }
+
+    public double getPrecio_unitario() {
+        return precio_unitario;
+    }
+
+    public void setPrecio_unitario(double precio_unitario) {
+        this.precio_unitario = precio_unitario;
+    }
+    
+    
     
 }
